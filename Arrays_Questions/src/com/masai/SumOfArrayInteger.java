c8f48c7fdce17560d1f53a10411466e3ce29147a
Your Name <fehmida1899@gmail.com>
2023-11-03 22:24:49 +0530

    sumofarray

---
+++ Arrays_Questions/src/com/masai/SumOfArrayInteger.java
@@ -0,0 +1,17 @@
+package com.masai;
+
+public class SumOfArrayInteger {
+	 public static void main(String[] args) {
+	        // System.out.println("Hello, World!");
+	        
+	        int[] arr = {2,4,4,6,6,1,5};
+	        
+	        int sum =0;
+	        for(int i=0;i<arr.length;i++){
+	            sum+=arr[i];
+	        }
+	        System.out.println(sum);
+	        
+	        
+	    }
+}
