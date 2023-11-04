fb0e5ee674e6109e4e933b58c88be88412bb64a6
Your Name <fehmida1899@gmail.com>
2023-11-04 12:53:39 +0530

    SmallestinArray

---
+++ Arrays_Questions/src/com/masai/SmallestElement.java
@@ -0,0 +1,16 @@
+package com.masai;
+
+public class SmallestElement {
+public static void main(String[] args) {
+	int arr[] = {10,20,80,30,-1};
+	
+	int min = arr[0];
+	for(int i=0;i<arr.length;i++) {
+		if(arr[i]<min) {
+			min =  arr[i];
+		}
+	}
+	System.out.println("Smallest number is : "+min);
+	
+}
+}
