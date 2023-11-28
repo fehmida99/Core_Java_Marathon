dca03e08f28bed13db7d952966dcdcda71b4daa7
Your Name <fehmida1899@gmail.com>
2023-11-28 08:57:05 +0530

    HashMap

---
+++ Collection Framework/src/com/HashMap/HasMapDemo.java
@@ -0,0 +1,22 @@
+package com.HashMap;
+
+import java.util.HashMap;
+
+public class HasMapDemo {
+public static void main(String[] args) {
+	HashMap<Integer, String> hm = new HashMap<>();
+	hm.put(101, "Hina");
+	hm.put(102, "Fehmida");
+	hm.put(103, "Arshi");
+	hm.put(105, "Sadiya");
+	
+	System.out.println(hm);
+	
+	
+	
+	
+	
+	
+	
+}
+}
