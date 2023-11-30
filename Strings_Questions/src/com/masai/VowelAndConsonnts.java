adef4025f58c9e0fc01a305a16f0b949b61339c7
Your Name <fehmida1899@gmail.com>
2023-11-30 21:03:26 +0530

    vowel

---
+++ Strings_Questions/src/com/masai/VowelAndConsonnts.java
@@ -0,0 +1,28 @@
+package com.masai;
+
+public class VowelAndConsonnts {
+public static void main(String[] args) {
+	
+	int vowelCount=0;
+	int c_count =0;
+	
+	
+	
+	String input = "fehmida";
+	for(int i=0;i<input.length();i++){
+		if(input.charAt(i)=='a' || input.charAt(i)=='e' || input.charAt(i)=='i' 
+				|| input.charAt(i)=='o' || input.charAt(i)=='u') {
+			vowelCount++;
+		}else {
+			c_count++;
+		}
+	}
+	
+	System.out.println("Vowels count :" + vowelCount);
+	System.out.println("Consonant count :" + c_count);
+	
+	
+	
+	
+}
+}
