e15a3ac42cbdbfdc6a92e78c9ea70ee8f34cb8da
Your Name <fehmida1899@gmail.com>
2023-12-01 23:54:46 +0530

    reverse

--- Strings_Questions/src/com/masai/Reverse_String.java
+++ Strings_Questions/src/com/masai/Reverse_String.java
@@ -1,15 +1,17 @@
 package com.masai;
 
 public class Reverse_String {
-public static void main(String[] args) {
-	String input = "fehmida";
-	String reverseString = "";
-	
-	for(int i= input.length()-1;i>=0;i--) {
-		reverseString+=input.charAt(i);
+	public static void main(String[] args) {
+
+		String input = "fehmida";
+		String revString = "";
+		for (int i = input.length() - 1; i >= 0; i--) {
+			revString += input.charAt(i);
+
+		}
+        System.out.println("Original String is : "+ input);
+        System.out.println();
+		System.out.println("reverse string  is : " + revString);
+
 	}
-	
-	System.out.println(reverseString);
-	
-}
 }
