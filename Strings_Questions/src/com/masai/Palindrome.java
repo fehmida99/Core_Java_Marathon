974efe5cb8ac4e186eb2e9ef02c6fe21e91d4877
Your Name <fehmida1899@gmail.com>
2023-12-02 00:17:38 +0530

    palindrome

---
+++ Strings_Questions/src/com/masai/Palindrome.java
@@ -0,0 +1,22 @@
+package com.masai;
+
+public class Palindrome {
+public static void main(String[] args) {
+	String input = "fehmida";
+	String revString = "";
+	
+	for(int i=input.length()-1;i>=0;i--) {
+		revString+=input.charAt(i);
+		
+	}
+	System.out.println(revString);
+	
+	if(input.equals(revString)) {
+		System.out.println("Palindrome");
+	}else {
+		System.out.println("Not a Palindrome");
+	}
+	
+	
+}
+}
--- Strings_Questions/src/com/masai/Reverse_String.java
+++ Strings_Questions/src/com/masai/Reverse_String.java
@@ -15,3 +15,5 @@ public class Reverse_String {
 
 	}
 }
+
+
